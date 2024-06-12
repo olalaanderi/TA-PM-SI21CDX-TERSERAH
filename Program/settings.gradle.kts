7db916dfb2ed49b8af87e3e60d06432c2e366529
olalaanderi <yolanda_anderi@teknokrat.ac.id>
2024-06-12 20:58:26 +0700

    Risa:Upload Code

---
+++ Program/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "banana_apps"
+include(":app")
+ 
