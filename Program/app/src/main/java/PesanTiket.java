c5d5f3fde2728c8e7d8df3b70a81b860143c8679
RisaFitriaApriyani <152594374+RisaFitriaApriyani@users.noreply.github.com>
2024-06-19 23:32:51 +0700

    Add files via upload

---
+++ Program/app/src/main/java/PesanTiket.java
@@ -0,0 +1,28 @@
+package com.example.tiketku.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.example.tiketku.R;
+
+public class PesanTiket extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesan_tiket);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Home = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(Home);
+            }
+        });
+    }
+}
